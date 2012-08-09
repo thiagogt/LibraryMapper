3d24050fdacb0195fb7b3f1308612d0c270627b1
thiagogt <thimakgt@gmail.com>
2012-08-08 21:15:42 -0300

    Refatorada a tela de apresentacao

--- WebContent/jquery-ui-1.8.21.custom/effect.js
+++ WebContent/jquery-ui-1.8.21.custom/effect.js
@@ -3,10 +3,10 @@ var actualX=0,actualY=0;
 $(function(){
 	$(".createButton").click( function() {
 		
-		$(".canvas").append( "<div class='bloco' id='bloco-"+idBloco+"'></div>");
+		$(".body").append( "<div class='bloco' id='bloco-"+idBloco+"'></div>");
 		actualX=$("#bloco-"+idBloco).width();
 		actualY=$("#bloco-"+idBloco).height();
-		$("#bloco-"+idBloco).draggable({ containment: [100, 100, 700, 400] , cursor: "crosshair" },
+		$("#bloco-"+idBloco).draggable({ cursor: "crosshair" },
 				{drag: function(event, ui) {
 			        // Show the current dragged position of image
 			        var currentPos = $(this).position();
