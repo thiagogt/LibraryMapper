131310c8c1b5eb5e6d3bbc3dcd7725f2b689be8b
thiagogt <thimakgt@gmail.com>
2012-08-10 17:59:35 -0300

    os botoes sao criados dentro do quadro; o zoom esta hidden

--- WebContent/jquery-ui-1.8.21.custom/effect.js
+++ WebContent/jquery-ui-1.8.21.custom/effect.js
@@ -1,9 +1,11 @@
 var idBloco = 0;
 var actualX=0,actualY=0;
+var escala = 1;
+var velocidade = 0.01;
 $(function(){
 	$(".createButton").click( function() {
 		
-		$(".body").append( "<div class='bloco' id='bloco-"+idBloco+"'></div>");
+		$(".canvas").append( "<div class='bloco' id='bloco-"+idBloco+"'></div>");
 		actualX=$("#bloco-"+idBloco).width();
 		actualY=$("#bloco-"+idBloco).height();
 		$("#bloco-"+idBloco).draggable({ cursor: "crosshair" },
@@ -13,6 +15,10 @@ $(function(){
 			        $(".label").text("Position: \nLeft: " + currentPos.left + "\nTop: " + currentPos.top);
 			        actualY = $(this).height();
 					actualX = $(this).width();
+					
+					if(currentPos.left == 100){
+						//cria o codigo dentro do canvas.....mas tava dando delay e pau
+					}
 			    }
 			}).resizable({resize: function(event, ui) { 
 				
@@ -27,9 +33,56 @@ $(function(){
 });
 
 jQuery(document).ready(function(){
+	
 	   $(document).mousemove(function(e){
 		   
 		   
 	      $('#status').html(actualX +', '+ actualY);
 	   }); 
+	  
+	   $('#quadro').bind('mousewheel', function(event,delta){
+		   
+		   var div = document.getElementById('quadro');
+		   var property = getTransformProperty(div);
+		   if (delta > 0) {
+		   	 $(".label").text("Position: just whell up"); 
+		   	if (property) {
+		   	    
+   	    		escala =escala-velocidade;
+   	            div.style[property] = 'scale('+escala+','+escala+')';
+   	    
+   		}
+		   
+		   } else {
+		   	 $(".label").text("Position: just whell down");
+		   	if (property) {
+		   	    
+		   	    		escala =escala+velocidade;
+		   	            div.style[property] = 'scale('+escala+','+escala+')';
+		   	    
+		   		}
+		   	 
+		   	
+		   	 
+		   	}
+	   });
 });
+
+function getTransformProperty(element) {
+    // Note that in some versions of IE9 it is critical that
+    // msTransform appear in this list before MozTransform
+    var properties = [
+        'transform',
+        'WebkitTransform',
+        'msTransform',
+        'MozTransform',
+        'OTransform'
+    ];
+    var p;
+    while (p = properties.shift()) {
+        if (typeof element.style[p] != 'undefined') {
+            return p;
+        }
+    }
+    return false;
+}
