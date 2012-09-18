dc0dca1630edf853b9b4a471d3d7b616029bc8a3
thiago <thiago@thiago-HP-Pavilion-dx6500-Notebook-PC.(none)>
2012-09-18 17:45:11 -0300

    adicionado o arquivo MapperConfig.xml

--- src/library/utils/SQLFactory.java
+++ src/library/utils/SQLFactory.java
@@ -1,14 +1,61 @@
 package library.utils;
 
 import java.sql.*;
+import org.apache.ibatis.io.Resources;
+
+import org.apache.ibatis.session.SqlSession;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class SQLFactory {
-	 public Connection getConnection() {
-	        try {
-	            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/libraryMapper","postgres","postgres");
-	        }
-	        catch(SQLException excecao) {
-	            throw new RuntimeException(excecao);
-	        }
-	    }
+	private static final SqlSessionFactory sqlSessionFactory;
+
+	static {
+
+	try {
+
+	Reader reader = Resources.getResourceAsReader("MapperConfig.xml");
+
+	sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+
+	} catch (Exception e) {
+
+	e.printStackTrace();
+
+	throw new RuntimeException("Error initializing MyAppSqlConfig class. Cause: " + e);
+
+	}
+
+	}
+
+	public static SqlSessionFactory getSqlSessionFactory() {
+
+	return sqlSessionFactory;
+
+	}
+
+	public static SqlSession abrirSessao(){
+
+	return getSqlSessionFactory().openSession();
+
+	}
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	//	 public Connection getConnection() {
+//	        try {
+//	            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/libraryMapper","postgres","postgres");
+//	        }
+//	        catch(SQLException excecao) {
+//	            throw new RuntimeException(excecao);
+//	        }
+//	    }
 }
