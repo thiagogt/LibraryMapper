766a3ccf948a9fbcdc03401fae49aa5e241e910d
Itai Soares <itaimac@Mac-mini-de-Itai.local>
2012-09-27 11:40:30 -0300

    dropabble arrumado

--- WebContent/WebLibraryMapper/js/effect.js
+++ WebContent/WebLibraryMapper/js/effect.js
@@ -8,36 +8,60 @@ $(function(){
 		$("body").append( "<div class='bloco' id='bloco-"+idBloco+"'></div>");
 		actualX=$("#bloco-"+idBloco).width();
 		actualY=$("#bloco-"+idBloco).height();
-		$("#bloco-"+idBloco).draggable({ revert: "invalid" },{ cursor: "crosshair" },
-				{ grid: [ 10,10 ] },
-				{ containment: 'parent'},
-				{drag: function(event, ui) {
-			        // Show the current dragged position of image
-			        var currentPos = $(this).position();
-			        $(".label").text("Position: \nLeft: " + currentPos.left + "\nTop: " + currentPos.top);
-			        actualY = $(this).height();
-					actualX = $(this).width();
-					
-					if(currentPos.left == 100){
-						//cria o codigo dentro do canvas.....mas tava dando delay e pau
-					}
-			    }
-			});
+		
+		
+		$("#bloco-"+idBloco).draggable({
+	        helper: 'clone',
+	        cursor: 'move',
+	        tolerance: 'fit',
+	        revert: "invalid"
+	        
+	    });
 		$("#DrawArea").droppable({
-				drop: function(event,ui){
-					$("#bloco-"+idBloco).resizable({ containment: "#DrawArea", scroll: false },
-							{ grid: [ 10,10 ] },
-							{resize: function(event, ui) { 
-								$("#status").text( actualX + ", "+actualY);
-								actualY = $(this).height();
-								actualX = $(this).width();
-						
-							}
-						});
+
+            drop: function (e, ui) {
+
+                if ($(ui.draggable)[0].id != "") {
+                    x = ui.helper.clone();
+                ui.helper.remove();
+                x.draggable({
+                    helper: 'original',
+                    containment: '#DrawArea',
+                    tolerance: 'fit',
+                    grid: [ 10,10 ],
+                    drag: function(event, ui) {
+    			        // Show the current dragged position of image
+    			        var currentPos = $(this).position();
+    			        $(".label").text("Position: \nLeft: " + currentPos.left + "\nTop: " + currentPos.top);
+    			        actualY = $(this).height();
+    					actualX = $(this).width();
+    					
+    					if(currentPos.left == 100){
+    						//cria o codigo dentro do canvas.....mas tava dando delay e pau
+    					}
+    			    }
+                });
+                x.resizable({
+                	containment: "#DrawArea", 
+                	scroll: false,
+                	grid: [ 10,10 ],
+                	resize: function(event, ui) { 
+						$("#status").text( actualX + ", "+actualY);
+						actualY = $(this).height();
+						actualX = $(this).width();
+				
 					}
-				});
+                });
+                x.appendTo('#DrawArea');
+            }
+
+            }
+        });
+
+
 		idBloco++;
 	});
+	
 });
 
 
